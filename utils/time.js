216ada42664b7f89c7b8237ffd42b6d3ebea634f
BrayanSGL <bsgaleanol@udistrital.edu.co>
2024-11-02 07:49:10 -0500

    add time.js

---
+++ utils/time.js
@@ -0,0 +1,11 @@
+function clock() {
+    const now = new Date();
+    const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+    const millisecondsSinceStartOfDay = now - startOfDay;
+    const newTime = millisecondsSinceStartOfDay * 48;
+    const newDate = new Date(startOfDay.getTime() + newTime);
+    return newDate.toISOString().slice(0, 19).replace('T', ' ');
+  }
+  
+  console.log(clock());
+  
