4ff9ec5e2ed5c4b3420dedc0b838de170d51012f
sebastian2000pk <juochoas@unal.edu.co>
2024-11-13 21:34:59 -0500

    test

--- utils/fligth.js
+++ utils/fligth.js
@@ -2,7 +2,7 @@ import { clock } from "./time";
 
 export const getActualFlight = (flights) => {
   // sumarle 16 dias a la fecha actual
-  const time = clock() + 16 * 24 * 60 * 60 * 1000;
+  const time = clock() - 16 * 24 * 60 * 60 * 1000;
   const actualFlight = flights.find(
     (flight) => time >= flight.departure && time <= flight.arrival
   );
