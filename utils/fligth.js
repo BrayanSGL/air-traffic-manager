49f768bc3fd26c0d87430dc57ae1f749891b20ea
sebastian2000pk <juochoas@unal.edu.co>
2024-11-13 21:16:03 -0500

    Test

--- utils/fligth.js
+++ utils/fligth.js
@@ -2,8 +2,14 @@ import { clock } from "./time";
 
 export const getActualFlight = (flights) => {
   const time = clock();
+  // const actualFlight = flights.find(
+  //   (flight) => time >= flight.departure && time <= flight.arrival
+  // );
+  // but ignore the day and month and year of the date
   const actualFlight = flights.find(
-    (flight) => time >= flight.departure && time <= flight.arrival
+    (flight) =>
+      time.slice(11) >= flight.departure.slice(11) &&
+      time.slice(11) <= flight.arrival.slice(11)
   );
   return actualFlight;
 };
