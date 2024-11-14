1896f6b3fcfe2d8306465ff6aa7a32aa5cf46543
sebastian2000pk <juochoas@unal.edu.co>
2024-11-13 21:36:46 -0500

    tset

--- utils/fligth.js
+++ utils/fligth.js
@@ -4,7 +4,11 @@ export const getActualFlight = (flights) => {
   // sumarle 16 dias a la fecha actual
   const time = clock() - 16 * 24 * 60 * 60 * 1000;
   const actualFlight = flights.find(
-    (flight) => time >= flight.departure && time <= flight.arrival
+    (flight) => {
+      console.log(flight.departure, flight.arrival);
+      console.log(time);
+      return time >= flight.departure && time <= flight.arrival
+    }
   );
   return actualFlight;
 };
