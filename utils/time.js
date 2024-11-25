9f932d053378a7649a11e1ca33524dfbbb697866
sebastian2000pk <juochoas@unal.edu.co>
2024-11-24 19:16:32 -0500

    time

--- utils/time.js
+++ utils/time.js
@@ -1,7 +1,7 @@
 export function clock() {
   const now = new Date();
   const startOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate());
-  const millisecondsSinceStartOfDay = now - startOfDay;
+  const millisecondsSinceStartOfDay = now.getTime() - startOfDay.getTime();
   const newTime = millisecondsSinceStartOfDay * 48;
   const newDate = new Date(startOfDay.getTime() + newTime);
   return newDate.toISOString().slice(0, 19).replace("T", " ");
